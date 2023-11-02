fc41ba48e466383ec53f85603e1197d580da8e26
Seid  Krsic <seidkrsic@Seids-MacBook-Air.local>
2023-11-02 13:48:43 +0100

    fix18

--- user_app/models.py
+++ user_app/models.py
@@ -143,7 +143,7 @@ class Profile(models.Model):
     commission = models.BooleanField(default=False, blank=True, null=True)
     bio = models.TextField(null=True) 
     bio_cyrillic= models.TextField(blank=True, null=True) 
-    profile_image = models.ImageField(blank=True, null=True, upload_to='', default=f"ArtBoard_2.png")
+    profile_image = models.ImageField(blank=True, null=True, upload_to='', default=f"Artboard_2.png")
     created = models.DateTimeField(auto_now_add=True) 
     id = models.UUIDField(default=uuid.uuid4,unique=True, 
                                     primary_key=True, editable=False) 
