acc0b439d1386825ca73a7d142830861418cc6ec
Seid  Krsic <seidkrsic@Seids-Air.netis.cc>
2023-12-26 11:49:46 +0100

    new_year7

--- sindikat_app/admin.py
+++ sindikat_app/admin.py
@@ -180,7 +180,7 @@ class NewsAdmin(admin.ModelAdmin):
             file_format = file.content_type
             print("File format:", file_format)
             if file_format in ['image/jpeg', 'image/jpg', 'image/png']:
-                    news_image = Image(news=obj, image_url=f'{file.image_url}')
+                    news_image = Image(news=obj, image_url=f'{file.url}')
                     news_image.save()
             else:
                 print("Skipping non-image file:", file)
